26e6704d557f7c4563ca6b5a9009168e70e625ed
Davanum Srinivas <davanum@gmail.com>
2018-10-12 20:50:18 -0400

    Restore bodystring.go/jsonutils.go/statusgroup.go from SHA 4a31dd8
    Over time it looks like the license header was updated in some files.
    Here we restore the files to what was in git earlier back to the
    previous license. The files were picked from:
    https://github.com/heketi/heketi/tree/4a31dd8acc31f00227f5fb0909a79951ce1ae85d/pkg/utils
    
    using the following commands:
    git checkout 4a31dd8acc31f00227f5fb0909a79951ce1ae85d pkg/utils/bodystring.go
    git checkout 4a31dd8acc31f00227f5fb0909a79951ce1ae85d pkg/utils/jsonutils.go
    git checkout 4a31dd8acc31f00227f5fb0909a79951ce1ae85d pkg/utils/statusgroup.go
    
    Change-Id: I05eed41cfd90c304ae1981dec5c10dd293d7b5e6

--- pkg/utils/bodystring.go
+++ pkg/utils/bodystring.go
@@ -1,30 +1,35 @@
 //
 // Copyright (c) 2015 The heketi Authors
 //
-// This file is licensed to you under your choice of the GNU Lesser
-// General Public License, version 3 or any later version (LGPLv3 or
-// later), or the GNU General Public License, version 2 (GPLv2), in all
-// cases as published by the Free Software Foundation.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 package utils
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 // Return the body from a response as a string
 func GetStringFromResponse(r *http.Response) (string, error) {
 	body, err := ioutil.ReadAll(io.LimitReader(r.Body, r.ContentLength))
-	defer r.Body.Close()
 	if err != nil {
 		return "", err
 	}
+	r.Body.Close()
 	return string(body), nil
 }
 
@@ -33,11 +38,6 @@ func GetErrorFromResponse(r *http.Response) error {
 	s, err := GetStringFromResponse(r)
 	if err != nil {
 		return err
-	}
-
-	s = strings.TrimSpace(s)
-	if len(s) == 0 {
-		return fmt.Errorf("server did not provide a message (status %v: %v)", r.StatusCode, http.StatusText(r.StatusCode))
 	}
 	return errors.New(s)
 }
